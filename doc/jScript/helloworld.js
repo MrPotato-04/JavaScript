ed3dcfe1f08479d9567f1d5049d3b749dd8166b7
MrPotato-04 <80412415+MrPotato-04@users.noreply.github.com>
2021-06-09 14:05:42 +0200

    Initial commit

---
+++ doc/jScript/helloworld.js
@@ -0,0 +1,12 @@
+console.log("Hello world")
+
+for (var i = 0; i < 10; i++) {
+    console.log("de waarde van i is "+i)
+}
+var mijnAuto = {
+    merk: "ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+console.log("auto kleur is "+mijnAuto.kleur)
